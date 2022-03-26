5eb905ef0e3a0aed867d4027c2bfe9cee94b99e0
sikiryl <saritrat.206@gmail.com>
2022-03-26 09:32:53 +0700

    feat: adjust hello controller

--- src/main/java/metric/controller/HelloController.java
+++ src/main/java/metric/controller/HelloController.java
@@ -1,13 +1,18 @@
 package metric.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping(value = "/api")
     public String getHello(){
+        logger.info("Hello KubeOps Log");
         return "hello kubeops";
     }
 }
